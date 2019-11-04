1fedba3cde9e852c76f835bcc2bcfcd9b8e9d6ef
charan.malla <charan.malla@cleartrip.com>
2019-11-04 18:21:18 +0530

    adding new directories

---
+++ src/pages/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <q-card class='bg-primary'>
+      <div class="bg-primary">
+      </div>
+    </q-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      imageSrc: '',
+      err: '',
+      status: ''
+    }
+  },
+  methods: {
+  }
+}
+</script>
