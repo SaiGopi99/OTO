5b4201aaf2a1c45b7134592543c2bea874de13bc
charan.malla <charan.malla@cleartrip.com>
2019-11-04 14:52:24 +0530

    adding project files

---
+++ quasar.conf.js
@@ -0,0 +1,212 @@
+// Configuration for your app
+// https://quasar.dev/quasar-cli/quasar-conf-js
+
+const DotEnv = require('dotenv')
+const webpack = require('webpack')
+const envparser = require('./config/envparser')
+
+const path = require('path')
+
+module.exports = function (ctx) {
+  return {
+    // app boot file (/src/boot)
+    // --> boot files are part of "main.js"
+    boot: [
+      'axios'
+    ],
+
+    css: [
+      'app.styl'
+    ],
+
+    extras: [
+      // 'ionicons-v4',
+      // 'mdi-v3',
+      // 'fontawesome-v5',
+      // 'eva-icons',
+      // 'themify',
+      // 'roboto-font-latin-ext', // this or either 'roboto-font', NEVER both!
+
+      'roboto-font', // optional, you are not bound to it
+      'material-icons' // optional, you are not bound to it
+    ],
+
+    framework: {
+      // iconSet: 'ionicons-v4',
+      // lang: 'de', // Quasar language
+
+      // all: true, // --- includes everything; for dev only!
+
+      components: [
+        'QLayout',
+        'QHeader',
+        'QDrawer',
+        'QPageContainer',
+        'QPage',
+        'QCard',
+        'QBar',
+        'QSpace',
+        'QBanner',
+        'QBadge',
+        'QSlider',
+        'QTooltip',
+        'QToolbar',
+        'QToolbarTitle',
+        'QTable',
+        'QTr',
+        'QTh',
+        'QTd',
+        'QBtn',
+        'QIcon',
+        'QInput',
+        'QDialog',
+        'QList',
+        'QItem',
+        'QItemSection',
+        'QItemLabel',
+        'QMenu',
+        'QCardSection',
+        'QSelect',
+        'QChip',
+        'QCardActions',
+        'QBtnDropdown',
+        'QFab',
+        'QFabAction',
+        'QPageSticky',
+        'QPullToRefresh'
+      ],
+
+      directives: [
+        'Ripple',
+        'ClosePopup'
+      ],
+
+      // Quasar plugins
+      plugins: [
+        'Notify',
+        'SessionStorage',
+        'Cookies',
+        'Loading',
+        'Dialog',
+        'LocalStorage'
+      ]
+    },
+
+    supportIE: false,
+
+    build: {
+      scopeHoisting: true,
+      // vueRouterMode: 'history',
+      env: envparser(),
+      // vueCompiler: true,
+      // gzip: true,
+      // analyze: true,
+      // extractCSS: false,
+      extendWebpack (cfg) {
+        cfg.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /node_modules/,
+          options: {
+            formatter: require('eslint').CLIEngine.getFormatter('stylish')
+          }
+        })
+        cfg.resolve.alias.env = path.resolve(__dirname, 'config/helpers/env.js')
+
+        cfg.plugins.push(
+          new webpack.ProvidePlugin({
+            'env': 'env'
+          })
+        )
+      }
+    },
+
+    devServer: {
+      // https: true,
+      // port: 8080,
+      open: true // opens browser window automatically
+    },
+
+    // animations: 'all', // --- includes all animations
+    animations: [],
+
+    ssr: {
+      pwa: false
+    },
+
+    pwa: {
+      // workboxPluginMode: 'InjectManifest',
+      // workboxOptions: {}, // only for NON InjectManifest
+      manifest: {
+        // name: 'Stockone',
+        // short_name: 'Stockone',
+        // description: 'Barcode Scanner',
+        display: 'standalone',
+        orientation: 'portrait',
+        background_color: '#ffffff',
+        theme_color: '#027be3',
+        icons: [
+          {
+            'src': 'statics/icons/icon-128x128.png',
+            'sizes': '128x128',
+            'type': 'image/png'
+          },
+          {
+            'src': 'statics/icons/icon-192x192.png',
+            'sizes': '192x192',
+            'type': 'image/png'
+          },
+          {
+            'src': 'statics/icons/icon-256x256.png',
+            'sizes': '256x256',
+            'type': 'image/png'
+          },
+          {
+            'src': 'statics/icons/icon-384x384.png',
+            'sizes': '384x384',
+            'type': 'image/png'
+          },
+          {
+            'src': 'statics/icons/icon-512x512.png',
+            'sizes': '512x512',
+            'type': 'image/png'
+          }
+        ]
+      }
+    },
+
+    cordova: {
+      id: 'org.stockone.cordova.app',
+      // noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
+    },
+
+    electron: {
+      // bundler: 'builder', // or 'packager'
+
+      extendWebpack (cfg) {
+        // do something with Electron main process Webpack cfg
+        // chainWebpack also available besides this extendWebpack
+      },
+
+      packager: {
+        // https://github.com/electron-userland/electron-packager/blob/master/docs/api.md#options
+
+        // OS X / Mac App Store
+        // appBundleId: '',
+        // appCategoryType: '',
+        // osxSign: '',
+        // protocol: 'myapp://path',
+
+        // Windows only
+        // win32metadata: { ... }
+      },
+
+      builder: {
+        // https://www.electron.build/configuration/configuration
+
+        // appId: 'so_bosch'
+      }
+    }
+  }
+}
---
+++ src/pages/qc.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-card class = 'bg-warning'>
+    <div class='row'>
+    <div  v-for='(key,value) in db_data' :key ='value'>
+      <q-card-section class='q-ma-sm bg-primary text-white'>
+        <p class="text-bold">{{ value }}</p>
+      </q-card-section>
+      <draggable class='list-group' :list='key' group='people' @change='log'>
+        <div
+          class='list-group-item'
+          v-for='element in key'
+          :key='element.name'
+        >
+        <q-card-section class='q-ma-sm bg-white text-black'>
+          <q-input ref="input" v-model="element['name']" autofocus />
+          </q-card-section>
+        </div>
+        <q-btn round color="primary" icon="add" @click ="add_section('Add Card',true,value)" />
+      </draggable>
+    </div>
+    <div class='col-3'>
+       <q-btn class="q-ma-xl"  color="primary" label="Add New Section" @click ="add_section('Add Section',false,'')" />
+    </div>
+  </div>
+
+  </q-card>
+</template>
+
+<script>
+import draggable from 'vuedraggable'
+export default {
+  name: 'two-lists',
+  display: 'Two Lists',
+  order: 1,
+  components: {
+    draggable
+  },
+  data () {
+    return {
+      db_data: {}
+    }
+  },
+  mounted () {
+  },
+  methods: {
+    add () {
+      this.list.push({ name: 'Juan' })
+    },
+    replace () {
+      this.list = [{ name: 'Edgard' }]
+    },
+    clone (el) {
+      return {
+        name: el.name + ' cloned'
+      }
+    },
+    log (evt) {
+      window.console.log(evt)
+    },
+    add_new_record (data, check, colParam) {
+      var that = this
+      if (check) {
+        let len = that.db_data[colParam].length
+        that.$set(that.db_data[colParam], len, { 'name': data })
+      } else {
+        that.$set(that.db_data, data, [])
+        let len = that.db_data[data].length
+        that.$set(that.db_data[data], len, { 'name': '' })
+      }
+    },
+    add_section (title, check, colParam) {
+      var that = this
+      that.$q.dialog({
+        title: title,
+        message: 'Enter the Content Here',
+        cancel: true,
+        prompt: {
+          model: '',
+          type: 'text'
+        },
+        persistent: true
+      }).onOk(data => {
+        that.add_new_record(data, check, colParam)
+      }).onCancel(() => {
+      }).onDismiss(() => {
+      })
+    }
+  }
+}
+</script>
